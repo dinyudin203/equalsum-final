0c96f3582d1fa77e2d9f1dd213c240a7399ca1bb
sebiinim <142967491+sebiinim@users.noreply.github.com>
2024-12-16 15:39:28 +0900

    Update llm_select.py
    GPT로 모델 변경

--- equalsum-backend/app/ai/llm_select.py
+++ equalsum-backend/app/ai/llm_select.py
@@ -3,7 +3,7 @@ from ai.llm_gemini import gemini
 
 
 def select_llm():
-    user_input = 1
+    user_input = 0
     # user_input = int(input("사용할 llm을 입력하세요(gpt는 0, gemini는 1) : "))
     if user_input == 0:
         llm = gpt
